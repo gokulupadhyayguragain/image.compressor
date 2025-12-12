537ae2fbf7b0f88094d63d7df1db631495af3f6b
Gokul Upadhyay Guragain <gokulupadhyayguragain@gmail.com>
2025-12-12 19:43:22 +0545

    Add logging to entrypoint script for debugging

--- frontend/docker-entrypoint.d/99-copy-config.sh
+++ frontend/docker-entrypoint.d/99-copy-config.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 # Copy custom nginx config after entrypoint modifications
+echo "Copying custom nginx config..."
 cp /tmp/my-default.conf /etc/nginx/conf.d/default.conf
+echo "Config copied successfully"
