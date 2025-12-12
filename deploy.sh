b9fbb061e11b2bfc62114b2052fef131012ead29
Gokul Upadhyay Guragain <gokulupadhyayguragain@gmail.com>
2025-12-12 17:21:27 +0545

    FINAL DEPLOY

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,6 @@ clear
 git init
 git add .
 git commit -m "FINAL DEPLOY"
+git remote add origin git@github.com:gokulupadhyayguragain/image.compressor.git
+git branch -M main
+git push -u origin main
---
+++ github.key.sh
@@ -0,0 +1,5 @@
+# generate if needed
+ssh-keygen -t ed25519 -C "gokulupadhyayguragain@gmail.com"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_ed25519
+cat ~/.ssh/id_ed25519.pub
